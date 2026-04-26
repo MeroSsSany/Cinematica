b9d9902062490fea1f97b331c1eff76fdff824d4
MeroSsSany <meromina1371@gmail.com>
2026-04-26 03:11:23 +0300

    Fixed Imports

--- src/main/java/dev/merosssany/cinematica/core/Cinematica.java
+++ src/main/java/dev/merosssany/cinematica/core/Cinematica.java
@@ -1,11 +1,8 @@
 package dev.merosssany.cinematica.core;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mojang.logging.LogUtils;
-import dev.merosssany.cinematica.core.data.handler.JsonDataAdapter;
 import dev.merosssany.cinematica.core.data.scrollingtext.CreditsSettings;
 import dev.merosssany.cinematica.core.data.slideshow.SlideshowSettings;
 import dev.merosssany.cinematica.ObjectKey;
