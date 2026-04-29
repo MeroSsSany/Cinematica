ce4f6978644c453963c8d897ba23f6da7fb5f538
MeroSsSany <meromina1371@gmail.com>
2026-04-29 19:43:42 +0300

    Added open method in CinematicaCommands.java

--- src/main/java/dev/merosssany/cinematica/registry/command/CinematicaCommands.java
+++ src/main/java/dev/merosssany/cinematica/registry/command/CinematicaCommands.java
@@ -3,12 +3,16 @@ package dev.merosssany.cinematica.registry.command;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dev.merosssany.cinematica.core.data.slideshow.SlideshowSettings;
 import dev.merosssany.cinematica.networking.NetworkManager;
 import dev.merosssany.cinematica.networking.packet.OpenSlideshowPacket;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.Collection;
 
 public class CinematicaCommands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -28,6 +32,17 @@ public class CinematicaCommands {
                                         )
                         )
         );
+    }
+    
+    private static int open(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        Collection<ServerPlayer> players = EntityArgument.getPlayers(context, "players");
+        SlideshowSettings settings = context.getArgument("name", SlideshowSettings.class);
+        
+        for (ServerPlayer player : players) {
+            NetworkManager.sendToPlayer(player, new OpenSlideshowPacket(settings));
+        }
+        
+        return Command.SINGLE_SUCCESS;
     }
     
     private static int openSelf(CommandContext<CommandSourceStack> context) {
